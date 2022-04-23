6f024495ebf3ad5eb09532c63db46c0a89468137
pankaj kumar <pankaj0882000@gmail.com>
2022-04-23 12:22:23 +0530

    structures theory 5 questions 23-04-22

---
+++ 9- Structures/04_initialize.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<string.h>
+struct employee{
+    int code;
+    float salary;
+    char name[20];
+
+};
+
+
+int main()
+{
+    struct employee pankaj ={100,70.11,"pankaj"};   //{code,salary,name}
+    struct employee nitish ={102,80.45,"nitish"};
+    struct employee abhishik ={105,90.65,"abhishik"};
+   // struct employee rahul ={0}; // agar sarai elements ko 0 set karna hai to yai kam kar jyiga essai sarai elements 0 sai intilize ho jyigai
+
+    printf("code is %d\n", pankaj.code);
+    printf("salary is %f\n", pankaj.salary);
+    printf("name is %s\n", pankaj.name);
+
+    printf("code is%d\n", nitish.code);
+    printf("salary is %f\n",nitish.salary);
+    printf("name is %s\n",nitish.name);
+
+    printf("code is %d\n",abhishik.code);
+    printf("salary is %f\n", abhishik.salary);
+    printf("name is %s\n", abhishik.name);
+
+
+
+
+    return 0;
+}
---
+++ 9- Structures/07_typedef.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<string.h>
+ typedef struct employee{     // typedef ak alais(alias ka matlab ak nic name) bana deta hai // typedef custam deta type banata hai
+    int code;
+    float salary;
+    char name[20];
+
+}emp;  // struct employee ko replace kar sakte hai emp sai  // emp iss program mai deta type hai
+
+void show(emp emp1){
+    printf("the code of employee is %d\n", emp1.code);
+    printf("the salary of employee is %f\n", emp1.salary);
+    printf("the name of employee is %s\n", emp1.name);
+}
+
+
+int main()
+{
+    // declear e1 and ptr
+     emp e1;
+    emp *ptr;
+
+    // ptr point kar raha hai e1 ko
+    ptr = &e1;
+
+    // set the number  values for e1
+    ptr->code =101;
+    ptr->salary =50.45;
+    strcpy(ptr->name,"pankaj");
+
+    show(e1);
+
+
+    
+    return 0;
+}
